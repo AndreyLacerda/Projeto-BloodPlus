011dbb3a93ff779ff5948a0f57fd9ad179f9c577
Andrey Camargo Lacerda <andrey-lacerda@hotmail.com>
2019-06-02 12:56:49 -0300

    Finalizando CRUDs

--- src/br/edu/ifsp/aluno/bd2a3/conexaosql/CRUDMatchComum.java
+++ src/br/edu/ifsp/aluno/bd2a3/conexaosql/CRUDMatchComum.java
@@ -1,5 +1,122 @@
 package br.edu.ifsp.aluno.bd2a3.conexaosql;
 
-public class CRUDMatchComum {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import br.edu.ifsp.aluno.bd2a3.matchs.MatchComum;
+
+public class CRUDMatchComum {
+	
+	public static boolean inserirMatchComum(MatchComum match) {
+		String connectionUrl = "jdbc:sqlite:DataBase/BloodPlusDB.db";
+		Statement stmt = null;
+		
+		boolean testeConnection;
+		if (testeConnection = TesteConnectionSQLite.checarConexão()) {
+			try {
+				String insert = "INSERT INTO MatchComum VALUES ('"+match.getDoador().getEmail()+"', '"+match.getReceptor().getEmail()+"', "
+						+ "'"+match.getDoacao()+"', "+match.isFracasso()+", "+match.isSucesso()+", "+match.getMotivo()+", "
+								+ ""+match.isCancelada()+", "+match.isFinalizada()+" )";
+				
+				Connection conn = DriverManager.getConnection(connectionUrl);
+				stmt = conn.createStatement();
+				stmt.execute(insert);
+				return true;
+				
+			} catch(SQLException esql) {
+				System.err.println("SQLException: " + esql.getMessage());
+				System.err.println("SQLState: " + esql.getSQLState());
+				System.err.println("VendorError: " + esql.getErrorCode());
+				return false;
+			} catch (Exception e) {
+				System.err.println("Erro: " + e.getMessage());
+				return false;
+			}
+			
+		} else {
+			return false;
+		}
+	}
+	
+	public static boolean deleteMatch(MatchComum match) {
+		String connectionUrl = "jdbc:sqlite:DataBase/BloodPlusDB.db";
+		Statement stmt = null;
+		
+		boolean testeConnection;
+		if (testeConnection = TesteConnectionSQLite.checarConexão()) {
+			try {
+				String delete = "DELETE FROM MatchComum WHERE email_doador ="+match.getDoador().getEmail()+" AND email_receptor = "+match.getReceptor().getEmail()+"";
+				Connection conn = DriverManager.getConnection(connectionUrl);
+				stmt = conn.createStatement();
+				stmt.execute(delete);
+				return true;
+			} catch(SQLException esql) {
+				System.err.println("SQLException: " + esql.getMessage());
+				System.err.println("SQLState: " + esql.getSQLState());
+				System.err.println("VendorError: " + esql.getErrorCode());
+				return false;
+			} catch (Exception e) {
+				System.err.println("Erro: " + e.getMessage());
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
+	public static boolean updateMatchComum(MatchComum match,String campo, String valor) {
+		String connectionUrl = "jdbc:sqlite:DataBase/BloodPlusDB.db";
+		Statement stmt = null;
+		
+		boolean testeConnection;
+		if (testeConnection = TesteConnectionSQLite.checarConexão()) {
+			try {
+				String update = "UPDATE MatchComum SET "+campo+" = "+valor+" WHERE email_doador = "+match.getDoador().getEmail()+" AND email_receptor = "+match.getReceptor().getEmail()+"";
+				Connection conn = DriverManager.getConnection(connectionUrl);
+				stmt = conn.createStatement();
+				stmt.execute(update);
+				return true;
+			} catch(SQLException esql) {
+				System.err.println("SQLException: " + esql.getMessage());
+				System.err.println("SQLState: " + esql.getSQLState());
+				System.err.println("VendorError: " + esql.getErrorCode());
+				return false;
+			} catch (Exception e) {
+				System.err.println("Erro: " + e.getMessage());
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
+	public static ResultSet selectMatchComum(String campo, String valor) {
+		String connectionUrl = "jdbc:sqlite:DataBase/BloodPlusDB.db";
+		Statement stmt = null;
+		
+		boolean testeConnection;
+		if (testeConnection = TesteConnectionSQLite.checarConexão()) {
+			try{
+				Connection conn = DriverManager.getConnection(connectionUrl);
+				String select = "SELECT * FROM MatchComum WHERE "+campo+" = "+valor+";";
+				stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(select);
+				
+				return rs;
+			} catch(SQLException esql) {
+				System.err.println("SQLException: " + esql.getMessage());
+				System.err.println("SQLState: " + esql.getSQLState());
+				System.err.println("VendorError: " + esql.getErrorCode());
+				return null;
+			} catch (Exception e) {
+				System.err.println("Erro: " + e.getMessage());
+				return null;
+			}
+		} else {
+			return null;
+		}
+	}
 }
--- src/br/edu/ifsp/aluno/bd2a3/conexaosql/CRUDMatchPessoaInst.java
+++ src/br/edu/ifsp/aluno/bd2a3/conexaosql/CRUDMatchPessoaInst.java
@@ -1,5 +1,122 @@
 package br.edu.ifsp.aluno.bd2a3.conexaosql;
 
-public class CRUDMatchPessoaInst {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import br.edu.ifsp.aluno.bd2a3.matchs.MatchPessoaInst;
+
+public class CRUDMatchPessoaInst {
+	
+	public static boolean inserirMatchPessoaInst(MatchPessoaInst match) {
+		String connectionUrl = "jdbc:sqlite:DataBase/BloodPlusDB.db";
+		Statement stmt = null;
+		
+		boolean testeConnection;
+		if (testeConnection = TesteConnectionSQLite.checarConexão()) {
+			try {
+				String insert = "INSERT INTO MatchPessoaInst VALUES ('"+match.getDoador().getEmail()+"', '"+match.getReceptor().getEmail()+"', "
+						+ "'"+match.getDoacao()+"', "+match.getMotivo()+", "+match.isFracasso()+", "+match.isSucesso()+", "
+								+ ""+match.isCancelada()+", "+match.isFinalizada()+" )";
+				
+				Connection conn = DriverManager.getConnection(connectionUrl);
+				stmt = conn.createStatement();
+				stmt.execute(insert);
+				return true;
+				
+			} catch(SQLException esql) {
+				System.err.println("SQLException: " + esql.getMessage());
+				System.err.println("SQLState: " + esql.getSQLState());
+				System.err.println("VendorError: " + esql.getErrorCode());
+				return false;
+			} catch (Exception e) {
+				System.err.println("Erro: " + e.getMessage());
+				return false;
+			}
+			
+		} else {
+			return false;
+		}
+	}
+	
+	public static boolean deleteMatch(MatchPessoaInst match) {
+		String connectionUrl = "jdbc:sqlite:DataBase/BloodPlusDB.db";
+		Statement stmt = null;
+		
+		boolean testeConnection;
+		if (testeConnection = TesteConnectionSQLite.checarConexão()) {
+			try {
+				String delete = "DELETE FROM MatchPessoaInst WHERE email_doador ="+match.getDoador().getEmail()+" AND email_receptorJuridico = "+match.getReceptor().getEmail()+"";
+				Connection conn = DriverManager.getConnection(connectionUrl);
+				stmt = conn.createStatement();
+				stmt.execute(delete);
+				return true;
+			} catch(SQLException esql) {
+				System.err.println("SQLException: " + esql.getMessage());
+				System.err.println("SQLState: " + esql.getSQLState());
+				System.err.println("VendorError: " + esql.getErrorCode());
+				return false;
+			} catch (Exception e) {
+				System.err.println("Erro: " + e.getMessage());
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
+	public static boolean updateMatchPessoaInst(MatchPessoaInst match,String campo, String valor) {
+		String connectionUrl = "jdbc:sqlite:DataBase/BloodPlusDB.db";
+		Statement stmt = null;
+		
+		boolean testeConnection;
+		if (testeConnection = TesteConnectionSQLite.checarConexão()) {
+			try {
+				String update = "UPDATE MatchPessoaInst SET "+campo+" = "+valor+" WHERE email_doador = "+match.getDoador().getEmail()+" AND email_receptorJuridico = "+match.getReceptor().getEmail()+"";
+				Connection conn = DriverManager.getConnection(connectionUrl);
+				stmt = conn.createStatement();
+				stmt.execute(update);
+				return true;
+			} catch(SQLException esql) {
+				System.err.println("SQLException: " + esql.getMessage());
+				System.err.println("SQLState: " + esql.getSQLState());
+				System.err.println("VendorError: " + esql.getErrorCode());
+				return false;
+			} catch (Exception e) {
+				System.err.println("Erro: " + e.getMessage());
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
+	public static ResultSet selectMatchPessoaInst(String campo, String valor) {
+		String connectionUrl = "jdbc:sqlite:DataBase/BloodPlusDB.db";
+		Statement stmt = null;
+		
+		boolean testeConnection;
+		if (testeConnection = TesteConnectionSQLite.checarConexão()) {
+			try{
+				Connection conn = DriverManager.getConnection(connectionUrl);
+				String select = "SELECT * FROM MatchPessoaInst WHERE "+campo+" = "+valor+";";
+				stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(select);
+				
+				return rs;
+			} catch(SQLException esql) {
+				System.err.println("SQLException: " + esql.getMessage());
+				System.err.println("SQLState: " + esql.getSQLState());
+				System.err.println("VendorError: " + esql.getErrorCode());
+				return null;
+			} catch (Exception e) {
+				System.err.println("Erro: " + e.getMessage());
+				return null;
+			}
+		} else {
+			return null;
+		}
+	}
 }

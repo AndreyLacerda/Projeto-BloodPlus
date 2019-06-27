f76dc31688beae61dd11a3c83f8555be59296c2a
AndreyLacerda <andrey-lacerda@hotmail.com>
2019-06-27 00:27:29 -0300

    Tentando arrumar bug do Maven

--- src/br/edu/ifsp/aluno/bd2a3/javafx/App.java
+++ src/br/edu/ifsp/aluno/bd2a3/javafx/App.java
@@ -12,7 +12,7 @@ public class App extends Application{
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("MainScreenFXML.fxml"));
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainScreenFXML.fxml"));
 		stage.getIcons().add(new Image("/logo.png"));
 		stage.setTitle("Blood+");
 		Scene scene = new Scene(root);
